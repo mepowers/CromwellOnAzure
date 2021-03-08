f87a65b53808a7d7737a12d717733ba4ea187f90
Marissa <30879508+mepowers@users.noreply.github.com>
2021-03-08 08:58:20 -0800

    Adding Python script to query CosmosDB database
    Adding Python script to query CosmosDB database

---
+++ azure-cosmos-query.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 09:08:28 2021
+
+@author: mepowers
+"""
+
+from azure.cosmos import cosmos_client
+
+url = "https://{coa-AccountName}.documents.azure.com:{port}/"
+key = '{Connection-String}'
+database_name = 'TES'
+container_name = 'Tasks'
+stored_procedure = 'coa-demo'
+
+client = cosmos_client.CosmosClient(url, key)
+database = client.get_database_client(database_name)
+container = database.get_container_client(container_name)
+
+result = container.scripts.execute_stored_procedure(sproc=stored_procedure, partition_key='01') 
+
+print(result)
