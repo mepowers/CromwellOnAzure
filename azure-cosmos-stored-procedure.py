05d0e3d8b32ac3a782e01cd01f4fdb8fa1133423
Marissa <30879508+mepowers@users.noreply.github.com>
2021-03-12 07:27:31 -0800

    Update azure-cosmos-stored-procedure.py

--- azure-cosmos-stored-procedure.py
+++ azure-cosmos-stored-procedure.py
@@ -5,6 +5,8 @@ Created on Mon Mar  1 09:08:28 2021
 @author: mepowers
 """
 
+#When run on local machine, script will authenticate with CosmosDB account, execute a specific Stored Procedure in that account, and return the output
+
 from azure.cosmos import cosmos_client
 
 ## Authenticate with CosmosDB
@@ -12,16 +14,12 @@ from azure.cosmos import cosmos_client
 # under "Authenticate with Client" at:  
 # https://pypi.org/project/azure-cosmos/
 
-# For /small/ubams test workflow in useast:
-# url = "https://coa-704349517.documents.azure.com:443/"
-# key = '7BBMzohWbqvYc80FDarL4DqdRckWC8NKDRl9EheYRwnXa40nzPHaubdmJYIF9DmU7W6na99p99XHRswBeaD1hw=='
+url = "<url>"
+key = '<key>'
 
-url = "https://coa-2ff32443a.documents.azure.com:443"
-key = 'H7Qgd4DYNABjS8J6ruHna2PEyyDPp5ZyrQmigByLn4nWjkFWanGK0jaXVrFOaW8GcrdnJZc0O2oHeU2mdxh92g=='
-
-database_name = 'TES'
-container_name = 'Tasks'
-stored_procedure = 'coa-demo' #Can call and execute an existing Stored Procedure
+database_name = '<database-name>'
+container_name = '<container-name>'
+stored_procedure = '<name-of-stored-procedure>' #Can call and execute an existing Stored Procedure
 
 client = cosmos_client.CosmosClient(url, key)
 database = client.get_database_client(database_name)
